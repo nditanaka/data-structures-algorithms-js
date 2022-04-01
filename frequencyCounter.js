187c7086c8db358abb317a506cf8102972fc12cd
Tanaka Chingonzo <16524495+nditanaka@users.noreply.github.com>
2022-04-01 14:46:14 -0400

    Create frequencyCounter.js

---
+++ frequencyCounter.js
@@ -0,0 +1,68 @@
+function naivefrequencyCounter1(arr1, arr2){
+    // if lengths of arr1 and arr2 are the same
+        // loop over arr1 and arr2 and check that each element in arr1 squared is contained in arr2
+        // if arr[i] is not in arr2 then set result to false
+        // else result = true
+    //  return result
+    // O(n^2) - this is the naive approach
+    let result = true
+    if(arr1.length === arr2.length){
+        for(let i = 0; i < arr1.length; i++){
+            for(let j = 0; j < arr2.length; j++){
+                if(arr1[i] ^ 2 === arr2[j]){
+                    break
+                } else{
+                    result = false
+                }
+            }
+        }
+        return result
+    }else{
+        result = false
+    }
+    return result
+}
+
+function naivefrequencyCounter2(arr1, arr2) {
+    // check if arr1 and arr2 have the same length and return false if they don't
+        // if they do, then loop over arr1 and check if arr2 contains arr1[i] ^ 2
+        // if it does, set this to correctIndex
+            // arr2.splice(correctIndex, 1) to remove the current element
+            // if it does not then return false
+        // return true
+    // O(n^2) - this is the naive approach
+    if(arr1.length !== arr2.length){return false}
+    for(let i = 0; i < arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1){return false}
+        arr2.splice(correctIndex, 1)
+    }
+    return true  
+}
+
+// Frequency counter pattern
+// asymptotic notation is O(4n) but comes down to O(n)
+function frequencyCounter(arr1, arr2) {
+    let count1 = {}
+    let count2 = {}
+    if (arr1.length !== arr2.length) {
+        return false 
+    }
+    for (let val of arr1) {
+        count1[val] = (count1[val] || 0) + 1
+    }
+    for (let val of arr2) {
+        count2[val] = (count2[val] || 0) + 1
+    }
+    for (let key in count1) {
+        if (!count1[key ** 2] === count2[key]) {
+            return false
+        }
+    }
+    for (let key in count2) {
+        if (!count2[key ** 2] === count1[key]) {
+            return false
+        }
+    }
+    return true
+}
